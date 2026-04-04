75f882a6b6d671547b4b7415466ecedf1d07742f
sohamGGs <sohamp6012@gmail.com>
2026-04-05 01:36:31 +0530

    add documentation and usage examples for API-key based URL shortening services

---
+++ Day-38/custom_api_config.py
@@ -0,0 +1,7 @@
+import pyshorteners
+
+# Replace with your actual token if you have one
+# s = pyshorteners.Shortener(api_key='YOUR_BITLY_TOKEN')
+# print(s.bitly.short('https://www.google.com'))
+
+print("Note: Most services require an API key passed into the constructor.")
